341a7891ff08fac2b5b9f382f9df0ff324050612
rnmora <89809182+rnmora@users.noreply.github.com>
2022-01-19 20:31:15 -0500

    Update funciones.py

--- RASPBERRY PI4_ARCHIVOS/funciones.py
+++ RASPBERRY PI4_ARCHIVOS/funciones.py
@@ -1,82 +1,31 @@
-import os
-import socket
-import requests
-from wiringpi import Serial
-from time import sleep
+# Modules
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from time import*
+from funciones import*
+from config import*
 
-baud = 9600
-ser  = Serial("/dev/serial0",baud)
-sleep(0.3)
-myip = socket.gethostbyname(socket.gethostname())
-print (myip)
+init()
 
-def recibir(echo = True):
- data = ""
+# Main function
+def main () :
+# Infinite loop
  while True:
-  input = ser.getchar()
-  if echo:
-   ser.putchar(input)
-  if input == "\r":
-   return (data)
-  data += input
- sleep(0.2)
-  
-def printsln(menss):
- ser.puts(menss+"\r")
- sleep(0.2)
+  mensaje = recibir()
+  k1 = 0
+  try:
+   k1 = int(mensaje)
+  except:
+   print("error")
+   pass
+  sleep(0.1)
+  prints("Concentracion: ")
+  printsln(str(k1))
+  payload = build_payload(VARIABLE_LABEL_1)
+  print("[INFO] Attemping to send data")
+  post_request(payload)
+  print("[INFO] finished")
+  sleep(0.1)  
 
-def prints(menss):
- ser.puts(menss)
- sleep(0.2)
-
-def servidor():
- Request = None
-
- class RequestHandler_httpd(BaseHTTPRequestHandler):
-   def do_GET(self):
-    global Request
-
-    messagetosend = bytes('Solicitando',"utf")
-    self.send_response(200)
-    self.send_header('Content-Type', 'text/plain')
-    self.send_header('Content-Length', len(messagetosend))
-    self.end_headers()
-    self.wfile.write(messagetosend)
-    Request = self.requestline
-    Request = Request[5 : int(len(Request)-9)]
-
-    #print(Request)
-    if Request == 'soldatos':
-      print('Los datos han sido enviados a la app')
-      valor=int(recibir())
-      firebase.put("/Dato","/dato1",valor)
- 
-def build_payload(variable_1):
-    value_1 = int(recibir()) #Recibo el dato de concentracion
-    print ("CONECTADO...")  
-    payload = {variable_1: value_1}
-    return payload
-
-def post_request(payload):
-    # Creates the headers for the HTTP requests
-    url = "http://industrial.api.ubidots.com"
-    url = "{}/api/v1.6/devices/{}".format(url, DEVICE_LABEL)
-    headers = {"X-Auth-Token": TOKEN, "Content-Type": "application/json"}
-
-    # Makes the HTTP requests
-    status = 400
-    attempts = 0
-    while status >= 400 and attempts <= 5:
-        req = requests.post(url=url, headers=headers, json=payload)
-        status = req.status_code
-        attempts += 1
-        sleep(1)
-
-    # Processes results
-    if status >= 400:
-        print("[ERROR] Could not send data after 5 attempts, please check \
-            your token credentials and internet connection")
-        return False
-
-    print("[INFO] request made properly, your device is updated")
-    return True
+# Command line execution
+if __name__ == '__main__' :
+ main()
